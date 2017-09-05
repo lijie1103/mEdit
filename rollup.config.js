9e48de7c0e0a62bc3d2ad8daa3452d8dc5c5173e
王喆 <nasa127@gmail.com>
2017-09-05 16:38:49 +0800

    重构打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,51 @@
-var rollup = require('rollup');
-var babel = require('rollup-plugin-babel');
-// var uglify = require('rollup-plugin-uglify');
+import replace from 'rollup-plugin-replace';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-    entry: 'src/index.js',
+    entry: 'src/index.jsx',
     dest: 'dist/js/index.min.js',
     format: 'iife',
-    sourceMap: 'inline',
+    moduleName: 'preact-widget',
+    // moduleId: 'preact-widget',
+    sourceMap: false,
+    treeshake: true,
     plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
         babel({
-            exclude: 'node_modules/**'
-        })
-    ]
-}
+            exclude: [
+                'node_modules/!(' +
+                'google-map-react|preact|preact-compat|react-redux' +
+                ')/**',
+            ]
+        }),
+        nodeResolve({
+            jsnext: true,
+            // module: false,
+            // browser: true,
+            main: true,
+            extensions: ['.js', '.jsx'],
+            // preferBuiltins: true,
+        }),
+        commonjs({
+            include: 'node_modules/**',
+            namedExports: {
+                'node_modules/preact/dist/preact.js': ['h', 'render', 'Component', 'cloneElement', 'options'],
+            },
+        }),
+        uglify({
+            compress: {
+                // screw_ie8: true,
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: false,
+        }),
+    ],
+};
